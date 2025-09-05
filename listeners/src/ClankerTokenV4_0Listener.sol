5ac10846ad21569a30bca114107dd72b776f9dcf
Christoph Niemann <christoph@dune.com>
2025-09-05 13:26:28 +0200

    Add filtering logic for retake tokens
    Signed-off-by: Christoph Niemann <christoph@dune.com>

--- listeners/src/ClankerTokenV4_0Listener.sol
+++ listeners/src/ClankerTokenV4_0Listener.sol
@@ -31,14 +31,44 @@ contract ClankerTokenV4_0Listener is ClankerTokenV4_0$OnTransferEvent {
         // get token context by calling the token context function
         string memory tokenContext = IClankerTokenV4_0(ctx.txn.call.callee()).context();
 
-        TransferData memory data = TransferData({
-            from: inputs.from,
-            to: inputs.to,
-            value: inputs.value,
-            txHash: ctx.txn.hash(),
-            tokenContext: tokenContext
-        });
+        // Only emit event if tokenContext contains "streamm deployment" in messageId
+        if (containsStreammDeployment(tokenContext)) {
+            TransferData memory data = TransferData({
+                from: inputs.from,
+                to: inputs.to,
+                value: inputs.value,
+                txHash: ctx.txn.hash(),
+                tokenContext: tokenContext
+            });
 
-        emit TransferV4_0_0(data);
+            emit TransferV4_0_0(data);
+        }
+    }
+
+    /// Check if the tokenContext contains "streamm deployment" in the messageId field
+    /// This is a simple string search since Solidity doesn't have JSON parsing
+    function containsStreammDeployment(string memory tokenContext) internal pure returns (bool) {
+        bytes memory contextBytes = bytes(tokenContext);
+        bytes memory searchPattern = bytes("streamm deployment");
+        
+        if (contextBytes.length < searchPattern.length) {
+            return false;
+        }
+        
+        // Simple substring search for "streamm deployment"
+        for (uint i = 0; i <= contextBytes.length - searchPattern.length; i++) {
+            bool found = true;
+            for (uint j = 0; j < searchPattern.length; j++) {
+                if (contextBytes[i + j] != searchPattern[j]) {
+                    found = false;
+                    break;
+                }
+            }
+            if (found) {
+                return true;
+            }
+        }
+        
+        return false;
     }
 }
